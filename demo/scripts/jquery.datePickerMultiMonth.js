bce75d7253dc512425811223d32bbd9bab683fa7
Kelvin Luck <git@kelvinluck.com>
2009-02-09 06:04:47 +0000

    Added the new datePickerMultiMonth plugin for the datePicker plugin!

---
+++ demo/scripts/jquery.datePickerMultiMonth.js
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2008 Kelvin Luck (http://www.kelvinluck.com/)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ **/
+
+(function($){
+    
+	$.fn.extend({
+		datePickerMultiMonth : function(s)
+		{
+			if (s.inline == false) {
+				throw new Error('Not yet implemented for popup calendars :(');
+			}
+			if (s.numMonths == undefined) {
+				throw new Error('Must set numMonths to call datePickerMultiMonth!');
+			}
+
+			var dps = $.extend({}, $.fn.datePicker.defaults, s);
+			
+			$dpmm = $(this);
+
+			$dpmm.html('');
+
+			var pickers = [];
+			for (var i=0; i<s.numMonths; i++)
+			{
+				(function(i) {
+					var first = i == 0;
+					var last = i == s.numMonths - 1;
+					var $date =  $('<div />')
+									.datePicker(dps)
+									.bind('dpMonthChanged',
+										function(event, displayedMonth, displayedYear)
+										{
+											if (first) {
+												$dpmm.trigger('dpMonthChanged', [displayedMonth, displayedYear]);
+											} else {
+												pickers[i-1].dpSetDisplayedMonth(displayedMonth-1, displayedYear);
+											}
+											if (!last)	{
+												pickers[i+1].dpSetDisplayedMonth(displayedMonth+1, displayedYear);
+											}
+											return false;
+										}
+									)
+									.bind(
+										'dateSelected',
+										function(event, date, $td, status)
+										{
+											if (first)	{
+												$dpmm.trigger('dateSelected', [date, $td, status]);
+											} else {
+												pickers[i-1].dpSetSelected(date.asString(), status, false);
+											}
+											if (!last)	{
+												pickers[i+1].dpSetSelected(date.asString(), status, false);
+											}
+											return false;
+										}
+									);
+					$('.dp-nav-prev', $date).css('display', first ? 'block' : 'none');
+					$('.dp-nav-next', $date).css('display', last ? 'block' : 'none');
+					pickers.push($date);
+					$dpmm.append($date);
+				})(i);
+			}
+			// dodgy hack so that I can set the month to the correct value and trigger a redraw of the other elements... 
+			pickers[0].dpSetDisplayedMonth(1, 3000);
+			// set the month to the correct value so the other date pickers get set to the correct months...
+			pickers[0].dpSetDisplayedMonth(Number(s.month), Number(s.year));
+
+			return this;
+		}
+	});
+	
+})(jQuery);
