06ae50f65e1a98a37b24c7cce27d6c9479a459cf
Kelvin Luck <git@kelvinluck.com>
2009-11-09 10:37:41 +0000

    Added new examples showing how to combine the date picker multi month with a form...

---
+++ demo/formFeedback.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>PHP Submitted values</title>
+	</head>
+	<body>
+		<h1>Submitted form values from PHP</h1>
+		<h2>$_POST</h2>
+		<pre><? print_r($_POST); ?></pre>
+		
+		<h2>$_GET</h2>
+		<pre><? print_r($_GET); ?></pre>
+	</body>
+</html>
